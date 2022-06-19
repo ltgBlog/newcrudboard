a2506ba5738298ec69ee13ae1f5096a308fc6031
ltg9612@naver.com <51878026+lee-taekyun@users.noreply.github.com>
2022-06-20 02:55:27 +0900

    자신의 댓글만 수정/삭제 구현 (+rds 연결)

--- src/main/java/ltg/crudBoard/controller/UserIndexController.java
+++ src/main/java/ltg/crudBoard/controller/UserIndexController.java
@@ -50,7 +50,9 @@ public class UserIndexController
     @GetMapping("/auth/join")
     public String join()
     {
-        return "/user/user_join";
+        return "user/user_join";
+
+        //return "user/user_join";
     }
 
     @PostMapping("/auth/joinProc")
