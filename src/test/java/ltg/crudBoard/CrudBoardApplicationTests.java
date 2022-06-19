ae83cacbc05562fdfcceaf6d12381047eba39c13
ltg9612@naver.com <51878026+lee-taekyun@users.noreply.github.com>
2022-06-19 21:52:12 +0900

    자신의 댓글만 수정/삭제 구현 (+rds 연결)

--- src/test/java/ltg/crudBoard/CrudBoardApplicationTests.java
+++ src/test/java/ltg/crudBoard/CrudBoardApplicationTests.java
@@ -3,7 +3,7 @@ package ltg.crudBoard;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CrudBoardApplicationTests {
 
 	@Test
