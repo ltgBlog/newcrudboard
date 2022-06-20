c29aff08b15388b79aa4978aecdaecc29cd1ea61
ltg9612@naver.com <51878026+lee-taekyun@users.noreply.github.com>
2022-06-20 21:26:23 +0900

    자신의 댓글만 수정/삭제 구현 (+rds 연결) + 글 등록 공백 알림 추가

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -36,40 +36,61 @@ var main = {
             writer: $('#writer').val(),
             content: $('#content').val()
         };
+        // 공백 및 빈 문자열 체크
+            if (!data.title || data.title.trim() === "" || !data.content || data.content.trim() === "")
+            {
+                alert("공백 또는 입력하지 않은 부분이 있습니다.");
+                return false;
+            }
+            else
+            {
+                $.ajax({
+                    type: 'POST',
+                    url: '/api/posts',
+                    dataType: 'json',
+                    contentType:'application/json; charset=utf-8',
+                    data: JSON.stringify(data)
+                }).done(function() {
+                    alert('글이 등록되었습니다.');
+                    window.location.href = '/';
+                }).fail(function (error) {
+                    alert(JSON.stringify(error));
+                });
+            }
 
-        $.ajax({
-            type: 'POST',
-            url: '/api/posts',
-            dataType: 'json',
-            contentType:'application/json; charset=utf-8',
-            data: JSON.stringify(data)
-        }).done(function() {
-            alert('글이 등록되었습니다.');
-            window.location.href = '/';
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        });
     },
     update : function () {
         var data = {
+            id: $('#id').val(),
             title: $('#title').val(),
             content: $('#content').val()
         };
 
-        var id = $('#id').val();
+        const check = confirm("수정하시겠습니까?");
+        if (check === true)
+        {
+            if (!data.title || data.title.trim() === "" || !data.content || data.content.trim() === "")
+            {
+                alert("공백 또는 입력하지 않은 부분이 있습니다.");
+                return false;
+            }
+            else{
+            $.ajax({
+                    type: 'PUT',
+                    url: '/api/posts/'+data.id,
+                    dataType: 'json',
+                    contentType:'application/json; charset=utf-8',
+                    data: JSON.stringify(data)
+                }).done(function() {
+                    alert('글이 수정되었습니다.');
+                    window.location.href = '/posts/read/' + data.id;
+                }).fail(function (error) {
+                    alert(JSON.stringify(error));
+                });
+            }
+        }
 
-        $.ajax({
-            type: 'PUT',
-            url: '/api/posts/'+id,
-            dataType: 'json',
-            contentType:'application/json; charset=utf-8',
-            data: JSON.stringify(data)
-        }).done(function() {
-            alert('글이 수정되었습니다.');
-            window.location.href = '/';
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
-        });
+
     },
     delete : function () {
         var id = $('#id').val();
